b6c1ca808e985e5a1faa6bcf3b6cee434d78ce88
wabrocker <billbrocker@gmail.com>
2024-12-27 15:30:09 -0500

    add Order Details Page Build

--- src/app/(shop)/orders/index.tsx
+++ src/app/(shop)/orders/index.tsx
@@ -2,7 +2,7 @@ import { FlatList, ListRenderItem, Pressable, StyleSheet, Text, View } from 'rea
 
 import { ORDERS } from '../../../../assets/orders'
 import { Order, OrderStatus } from '../../../../assets/types/order';
-import { Link } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 
 const statusDisplayText: Record<OrderStatus, string> = {
   Pending: 'Pending',
@@ -32,6 +32,7 @@ const renderItem: ListRenderItem<Order> = ({ item }) =>
 const Orders = () => {
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Orders' }} />
       <FlatList
         data={ORDERS}
         renderItem={renderItem}
