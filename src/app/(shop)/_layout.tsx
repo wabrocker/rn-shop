2a887a9e34fe56bb50946b65bec8a69cff6f2949
wabrocker <billbrocker@gmail.com>
2024-12-29 08:02:26 -0500

    add Supabase Auth Provider

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,7 +1,9 @@
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
+import { ActivityIndicator, StyleSheet } from "react-native";
 import { FontAwesome } from '@expo/vector-icons';
+
+import { useAuth } from "../../providers/auth-provider";
 
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
@@ -11,6 +13,11 @@ function TabBarIcon(props: {
 }
 
 const TabsLayout = () => {
+  const { session, mounting } = useAuth();
+
+  if (mounting) return <ActivityIndicator />;
+  if (!session) return <Redirect href='/auth' />;
+
   return (
     <SafeAreaView edges={['top']} style={styles.safeArea}>
       <Tabs
